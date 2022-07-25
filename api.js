784e5414e6580b0bb231c9ca1f144b73d338e40c
gkoberger <gkoberger@gmail.com>
2022-07-24 21:32:02 -0400

    Add delete method

--- api.js
+++ api.js
@@ -5,23 +5,27 @@ const readme = require("readmeio");
 
 const auth = require("./lib/auth");
 
-const vesta = require('./lib/vesta');
+const vesta = require("./lib/vesta");
 
-const request = require('request');
+const request = require("request");
 
-require('dotenv').config();
+require("dotenv").config();
 
 router.use(auth);
 
 router.use(
-  readme.metrics(process.env.README_KEY, req => ({
-    apiKey: req.user.apiKey,
-    label: req.user.label,
-    email: req.user.email,
-  }), {
-    // optional, enable in development mode
-    //development: true,
-  })
+  readme.metrics(
+    process.env.README_KEY,
+    (req) => ({
+      apiKey: req.user.apiKey,
+      label: req.user.label,
+      email: req.user.email,
+    }),
+    {
+      // optional, enable in development mode
+      //development: true,
+    }
+  )
 );
 
 /* @oas [post] /message
@@ -37,15 +41,32 @@ router.use(
  */
 
 router.post("/message", (req, res) => {
-  // TODO: Actually change the Vestasboard!
-  const v = vesta(req.body.color,req.body.text);
+  const v = vesta(req.body.color, req.body.text);
 
-  console.log({ form: v.vesta() })
-  request.post('https://rw.vestaboard.com/', { json: v.vesta(), headers: { 'X-Vestaboard-Read-Write-Key': process.env.VESTABOARD }}, (a, b, c) => {
-    console.log(a, b, c);
-    })
+  request.post("https://rw.vestaboard.com/", {
+    json: v.vesta(),
+    headers: { "X-Vestaboard-Read-Write-Key": process.env.VESTABOARD },
+  });
+  res.send({ result: v.api() });
+});
 
-  res.send({result: v.api() });
+/* @oas [delete] /message
+ * summary: Reset sign message
+ * description: This will reset the Vestaboard's message
+ * tags:
+ * - Messages
+ * security:
+ * - ApiKeyAuth: []
+ */
+
+router.post("/message", (req, res) => {
+  const v = vesta('blue', 'Post a message here! https://readme.nyc');
+
+  request.post("https://rw.vestaboard.com/", {
+    json: v.vesta(),
+    headers: { "X-Vestaboard-Read-Write-Key": process.env.VESTABOARD },
+  });
+  res.send({ result: v.api() });
 });
 
 module.exports = router;
