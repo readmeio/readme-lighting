aac8fa5ad72a183e1ae1ffc556a114c3b0abe508
gkoberger <gkoberger@gmail.com>
2022-07-25 23:17:29 -0400

    Add webhook

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = parseInt(process.env.PORT || "3999", 10);
 
+const readme = require('readmeio');
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
@@ -10,6 +12,27 @@ const api = require("./api");
 app.get("/", (req, res) => {
   // Redirect users to the docs!
   res.redirect("https://docs.readme.nyc");
+});
+
+const createApiKey = (email) => {
+  const hash = Buffer.from(email.trim()).toString('base64').replace(/=/g, '');
+  return `rdme_nyc_${hash}`;
+};
+
+app.post('/webhook', express.raw({ type: 'application/json' }), (req, res) => {
+  const signature = req.headers['readme-signature'];
+  const secret = process.env.README_KEY;
+
+  try {
+    // Verify signature
+    readme.verifyWebhook(req.body, signature, secret);
+  } catch (e) {
+    return res.sendStatus(401);
+  }
+
+  return res.json({
+    apiKey: createApiKey(req.body.email),
+  });
 });
 
 app.use("/api", api);
